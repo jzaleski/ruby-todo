5a06f6ae1c8fa2b6d5beda7a51644a7becdbe1b7
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-02-18 21:30:13 -0500

    Remove "description" from Todo model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ActiveRecord::Base
 	belongs_to :created_by_user, :class_name => 'User'
 	belongs_to :list
-	attr_accessible :created_by_user, :list, :summary, :description, :completed_at
+	attr_accessible :created_by_user, :list, :summary, :completed_at
 	validates :summary, :presence => true
 end
