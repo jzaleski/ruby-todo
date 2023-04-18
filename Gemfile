729bb58befbddc05b30d0ddeb175b68334040273
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-04-18 09:58:14 -0500

    Update ruby-version and dependencies

--- Gemfile
+++ Gemfile
@@ -1,30 +1,30 @@
 source 'https://rubygems.org'
 
-ruby '2.6.2'
+ruby '2.7.6'
 
-gem 'puma', '~> 3.12.0'
+gem 'puma', '~> 6.2.0'
 
 gem 'rails', '~> 5.2.0'
-gem 'pg', '~> 1.1.0'
-gem 'haml', '~> 5.1.0'
+gem 'pg', '~> 1.4.0'
+gem 'haml', '~> 6.1.0'
 
-gem 'jquery-rails', '~> 4.3.0'
+gem 'jquery-rails', '~> 4.5.0'
 gem 'jquery_mobile_rails', '~> 1.4.0'
 gem 'pry-rails', '~> 0.3.0'
 
 group :assets do
   gem 'coffee-rails', '~> 5.0.0'
-  gem 'sass-rails', '~> 5.0.0'
-  gem 'uglifier', '~> 4.1.0'
+  gem 'sass-rails', '~> 6.0.0'
+  gem 'uglifier', '~> 4.2.0'
 end
 
 group :development do
-  gem 'listen', '>= 3.0.5', '< 3.2'
-  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '~> 3.8.0'
+  gem 'web-console', '~> 3.7.0'
 end
 
 group :development, :test do
   gem 'rails-controller-testing', '~> 1.0.0'
   gem 'rubocop', '~> 0.69.0'
-  gem 'test-unit-rails', '~> 5.2.0'
+  gem 'test-unit-rails', '~> 7.0.0'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,40 +8,42 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130311123940) do
+ActiveRecord::Schema.define(version: 2013_03_11_123940) do
 
-  create_table "lists", :force => true do |t|
-    t.integer  "created_by_user_id"
-    t.string   "name",               :null => false
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.integer "created_by_user_id"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "lists_users", :id => false, :force => true do |t|
+  create_table "lists_users", id: false, force: :cascade do |t|
     t.integer "list_id"
     t.integer "user_id"
   end
 
-  create_table "todos", :force => true do |t|
-    t.integer  "created_by_user_id"
-    t.string   "summary",              :null => false
+  create_table "todos", force: :cascade do |t|
+    t.integer "created_by_user_id"
+    t.string "summary", null: false
     t.datetime "completed_at"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
-    t.integer  "list_id"
-    t.integer  "completed_by_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "list_id"
+    t.integer "completed_by_user_id"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",      :default => "", :null => false
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.string   "password",                   :null => false
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password", null: false
+    t.index ["email", "password"], name: "email_and_password"
+    t.index ["email"], name: "email", unique: true
   end
-
-  add_index "users", ["email", "password"], :name => "email_and_password"
-  add_index "users", ["email"], :name => "email", :unique => true
 
 end
